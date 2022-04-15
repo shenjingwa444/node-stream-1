4119090609baa1031f4dbe0bc51dfd2940a00c57
jasoncheng <shenjingwa444@qq.com>
2022-04-15 19:00:08 +0800

    变换流

--- gzip.js
+++ gzip.js
@@ -1,9 +1,20 @@
 const fs = require('fs')
 const zlib = require('zlib')
 const file = process.argv[2]
+const {Transform} = require('stream')
+
+const reportProcess = new Transform({
+  transform(chunk,encoding,callback){
+    process.stdout.write('.')
+    //this.push(chunk) 相当于在 callback 中写第二个参数
+    callback(null,chunk)
+  }
+})
+
 fs.createReadStream(file)
   .pipe(zlib.createGzip())
-  //每次有数据就添加一个 '.' ，以此来监听数据的传输次数
-  .on('data',()=>process.stdout.write('.'))
-  .pipe(fs.createWriteStream(file + ".gz"))
+  //每次来数据，就打一个 '.' ，然后将数据原封不动的传给下一个
+  //这样可以对数据进行无限的处理，很像 webpack 的 loader
+  .pipe(reportProcess)
+  .pipe(fs.createWriteStream(file + ".zz"))
   .on('finish',()=>console.log('done'))
