4699a3a4ddddfd715e2dfa90e54c17f43d8cd17f
jasoncheng <shenjingwa444@qq.com>
2022-04-15 18:48:53 +0800

    看数据传输进度

--- gzip.js
+++ gzip.js
@@ -3,4 +3,7 @@ const zlib = require('zlib')
 const file = process.argv[2]
 fs.createReadStream(file)
   .pipe(zlib.createGzip())
+  //每次有数据就添加一个 '.' ，以此来监听数据的传输次数
+  .on('data',()=>process.stdout.write('.'))
   .pipe(fs.createWriteStream(file + ".gz"))
+  .on('finish',()=>console.log('done'))
